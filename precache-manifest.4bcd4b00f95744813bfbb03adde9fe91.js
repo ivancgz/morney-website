49a287f439b001b671daae88a92e505c011b90a9
ivanchen <2225490226@qq.com>
2021-12-28 17:21:30 +0800

    deploy

---
+++ precache-manifest.4bcd4b00f95744813bfbb03adde9fe91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47731dff257b60edc39d",
+    "url": "/morney-website/css/app.d5bb9b47.css"
+  },
+  {
+    "revision": "a51b39eb09b76e150160368faa053522",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "47731dff257b60edc39d",
+    "url": "/morney-website/js/app.3fb31be3.js"
+  },
+  {
+    "revision": "230c7e1833489128214d",
+    "url": "/morney-website/js/chunk-vendors.c4ecfe41.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
